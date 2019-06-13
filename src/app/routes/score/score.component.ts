e8a178ccfb7675f1127e12472affa14b5624f61e
jfan <lygfanjiahui@gmail.com>
2019-06-13 16:11:26 +0200

    OK

--- src/app/routes/score/score.component.ts
+++ src/app/routes/score/score.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faSmile } from '@fortawesome/free-regular-svg-icons';
+import { QuizService } from 'src/app/quiz.service';
+import { runInThisContext } from 'vm';
+import { faPoo, faSmileWink } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-score',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoreComponent implements OnInit {
 
-  constructor() { }
+  icon=faSmile;
+  constructor(public quiz:QuizService) { }
 
   ngOnInit() {
+    if(this.quiz.progress.score === 0){
+      this.icon=faPoo
+    }
+    if(this.quiz.progress.score === this.quiz.current.questions.length){
+      this.icon=faSmileWink;
+    }
   }
 
 }
